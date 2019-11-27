4829ad398b06e2493c6db5b3adeca5895a99857a
i970107 <i970107@naver.com>
2019-11-27 14:44:54 +0900

    initial commit

---
+++ src/main/java/kr/ac/gnu/anse/exercise/Calc.java
@@ -0,0 +1,13 @@
+package kr.ac.gnu.anse.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calc 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/kr/ac/gnu/anse/exercise/CalcTest.java
@@ -0,0 +1,38 @@
+package kr.ac.gnu.anse.exercise;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalcTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CalcTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CalcTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
